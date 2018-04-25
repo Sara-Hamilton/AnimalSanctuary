e96890c786d3e83571b3671c2afc805ad2c27420
Sara Hamilton and Johnny Mayer <sara@indeterminate.net>
2018-04-25 11:17:24 -0700

    add models for Animal and Veterinarian

---
+++ AnimalSanctuary/Models/Veterinarian.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnimalSanctuary.Models
+{
+    [Table("Veterinarians")]
+    public class Veterinarian
+    {
+        public Veterinarian()
+        {
+            this.Animals = new HashSet<Animal>();
+        }
+
+        [Key]
+        public int VeterinarianId { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public virtual ICollection<Animal> Animals {  get; set; }
+
+        public override bool Equals(System.Object otherVeterinarian)
+        {
+            if (!(otherVeterinarian is Veterinarian))
+            {
+                return false;
+            }
+            else
+            {
+                Item newVeterinarian = (Veterinarian)otherVeterinarian;
+                return this.VeterinarianId.Equals(newVeterinarian.VeterinarianId);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.VeterinarianId.GetHashCode();
+        }
+    }
+}
